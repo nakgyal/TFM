518651c30b8cb1df3f19491d09596ebdf605df0b
Leo <leoarris@ucm.es>
2024-02-29 08:38:42 +0100

    Segmentation maps for morphology analysis with statmorph

---
+++ make_postage_stamps_seg.py
@@ -0,0 +1,98 @@
+'''
+Copyright 2023 Astrobiology Center (Madrid)
+
+This file software allows to create postage stamps of jwst images.
+
+@Author: Luca Costantin (lcostantin@cab.inta-csic.es)
+
+:SPDX-License-Identifier: GPL-3.0+
+
+:History:
+18 Feb 23:  version 1.0
+'''
+
+import os
+import argparse
+import numpy as np
+import astropy.units as u
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.nddata import Cutout2D
+from astropy.coordinates import SkyCoord
+
+__version__ = "1.0"
+__author__  = "Luca Costantin, Astrobiology Center (Madrid)"
+
+def postage_stamp(ra, dec, idx=None):
+	'''Create postage stamps with a fixed size.
+	'''	
+	position = SkyCoord(ra, dec, frame='fk5', unit='deg')
+
+	with fits.open(args.infile) as hdu:
+		header0    = hdu[0].header
+		header_SCI = hdu[0].header
+		frame_SCI  = hdu[0].data
+
+		wcs = WCS(header_SCI)
+
+	try:
+		SCI_cutout = Cutout2D(frame_SCI, position, args.xycut * u.arcsec, 
+								mode='strict', wcs=wcs)
+
+		hdu[0].header.update(SCI_cutout.wcs.to_header())
+
+		if args.coordfile is None:
+			outname = 'image_cutout'
+			if args.id is not None:
+				outname = f'{args.id}_cutout'
+		else:
+			outname = f'{idx}_cutout'
+
+		file_cutout = f'{os.path.abspath(os.getcwd())}/{outname}.fits'
+
+		hdu_header = fits.PrimaryHDU(header=header0)
+		hdu_SCI    = fits.ImageHDU(SCI_cutout.data, header=header_SCI)
+		hdu_cutout = fits.HDUList([hdu_header, hdu_SCI])
+
+		if (os.path.isfile(file_cutout) is False or args.overwrite is True):
+
+			hdu_cutout.writeto(file_cutout, overwrite=True)
+
+	except:
+		pass
+	
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Create postage stamps for jwst imaging data.')
+	parser.add_argument("--infile", help='"Name of input image file (default=None)"', nargs='?', type=str, default=None)
+	parser.add_argument("--id", help='"ID of the target (default: None)"', nargs='?', type=int, const=None, default=None)
+	parser.add_argument("--ra", help='"RA of the target in degree - fk5 (default: None)"', nargs='?', type=float, const=None, default=None)
+	parser.add_argument("--dec", help='"DEC of the target in degree - fk5 (default: None)"', nargs='?', type=float, const=None, default=None)
+	parser.add_argument("--coordfile", help='"File with ID, RA, DEC in degree - fk5 (default: None)"', nargs='?', type=str, default=None)
+	parser.add_argument("--xycut", help='"Size of the cutout in arcsec (default: 10)"', nargs='?', type=int, const=10, default=10)
+	parser.add_argument("--addframe", help='"Create ERR and WHT frames additional (default: False)"', nargs='?', type=bool, choices=(True, False), const=False, default=False)
+	parser.add_argument("--overwrite", help='"Overwrite previous results (default: False)"', nargs='?', type=bool, choices=(True, False), const=False, default=False)
+	args = parser.parse_args()
+
+	if (args.coordfile is not None):
+
+		idx = np.loadtxt(args.coordfile, usecols=0, dtype=int)
+		ra  = np.loadtxt(args.coordfile, usecols=1, dtype=float)
+		dec = np.loadtxt(args.coordfile, usecols=2, dtype=float)
+
+		for i in range(idx.size):
+
+			if (idx.size == 1):
+				postage_stamp(ra, dec, idx)
+
+			else:
+				postage_stamp(ra[i], dec[i], idx[i])
+
+	else:
+		
+		ra = args.ra 
+		dec = args.dec
+
+		postage_stamp(ra, dec)
+
